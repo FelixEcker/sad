9d4767677e94ae856c1e13c4763c9bba34bfb89a
FelixEcker <felix0eckert0.online.de@gmail.com>
2022-11-17 13:10:00 +0100

    Update windows install script

--- windows_install.bat
+++ windows_install.bat
@@ -1,12 +1,8 @@
 @echo off
 
-echo \> Building...
 fpc src\sadv.pas -O4 -FE"out/"
 
-echo \> Creating Install Direcotry
 mkdir "C:\Program Files\sadv"
-
-echo \> Copying sadv.exe to Install Directory
 mv out\sadv.exe "C:\Program Files\sadv\sadv.exe"
 
 pause
