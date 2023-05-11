89643e15a0ddbd1cce789b017182bde4a5c21db9
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-05-11 13:28:04 +0200

    parameter and structure parsing

--- sadhtml/src/sadhtml.pp
+++ sadhtml/src/sadhtml.pp
@@ -6,6 +6,8 @@ program sadhtml;
 { Author: Marie Eckert                               }
 { Licensed under the 3-Clause BSD License. See below }
 
+{ TODO: Potentially refactor the reused code from sadv into a shared unit }
+
 (* Copyright (c) 2022, Marie Eckert                                           *)
 (*                                                                            *)
 (* Redistribution and use in source and binary forms, with or without         *)
@@ -36,10 +38,18 @@ program sadhtml;
 
 {$H+}
 
-uses SysUtils, uSADHTML;
+uses SysUtils, uSADParser, uSADHTML;
 
 const
+  DEFAULT_STYLESHEET = '$XDG_CONFIG_HOME/sadhtml/default.css';
   VERSION = '1.0.0';
+var
+  i: Integer;
+  path, style_path, required_section, cparam: String;
+  converted: String;
+  print_meta: Boolean;
+  doc: TSADocument;
+  meta: TMetaData;
 begin
   if (ParamCount() = 0) then
   begin
@@ -53,4 +63,58 @@ begin
     halt;
   end;
 
+  path := ParamStr(1);
+
+  if not FileExists(path) then
+  begin
+    writeln('Input File not found: ', path);
+    halt;
+  end;
+
+  style_path := DEFAULT_STYLESHEET;
+  required_section := '.';
+  print_meta := False;
+  for i := 2 to ParamCount() do
+  begin
+    cparam := ParamStr(i);
+    if cparam[1] = ':' then
+    begin
+      required_section := Copy(cparam, 2, Length(cparam));
+      break;
+    end;
+
+    if (cparam = '-m') or (cparam = '--meta') then
+      print_meta := True
+    else if (cparam = '-s') or (cparam = '--style') then
+    begin
+      if ParamCount() <= i+1 then
+      begin
+        writeln(cparam, ' requires one additional parameter: path to file');
+        exit;
+      end;
+
+      style_path := ParamStr(i+1);
+    end;
+  end;
+
+  if not FileExists(style_path) then
+  begin
+    writeln('Stylesheet file not found: ', style_path);
+    exit;
+  end;
+
+  Assign(doc.doc_file, path);
+  ReSet(doc.doc_file);
+  if not ParseStructure(doc) then
+  begin
+    writeln('ParseStructure failed on line ', doc.line_number);
+    writeln('--> ', parse_error);
+    halt;
+  end;
+
+{$IFDEF DEBUG}
+  DebugPrintDocument(doc);
+{$ENDIF}
+
+  {converted := HTMLParseSection();}
 end.
