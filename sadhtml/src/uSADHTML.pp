0425381c33e6b9497469788740f7967b3d5428c4
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-05-12 10:08:21 +0200

    Fix some bugs and add FindSection func

--- sadhtml/src/uSADHTML.pp
+++ sadhtml/src/uSADHTML.pp
@@ -41,7 +41,7 @@ interface
                             const ADoChildren: Boolean): String;
 implementation
   { Private Procedures/Functions }
-  
+
   procedure AppendToStrArray(var AArray: TStringDynArray; const AStr: String);
   begin
     SetLength(AArray, Length(AArray)+1);
@@ -54,11 +54,12 @@ implementation
                             const ADoChildren: Boolean): String;
   var
     lines, line_split, styles, colors: TStringDynArray;
-    current_line: String;
-    i, skip: Integer;
+    preserve_mode, current_line, tmp: String;
+    i, j, skip: Integer;
     section: TSection;
   begin
     HTMLParseSection := '';
+    preserve_mode := ASection.owning_document^.preserve_mode;
 
     lines := SplitString(ASection.contents, sLineBreak);
 
@@ -78,33 +79,87 @@ implementation
         case line_split[i] of
         HEADER: begin
           skip := Length(line_split);
+          HTMLParseSection := HTMLParseSection + '<h2>' +
+                                MergeStringArray(
+                                Copy(line_split, i+1, Length(line_split)-1),
+                                ' '
+                              ) + '</h1>';
         end;
         SUB_HEADER: begin
           skip := Length(line_split);
+          HTMLParseSection := HTMLParseSection + '<h3>' +
+                                MergeStringArray(
+                                Copy(line_split, i+1, Length(line_split)-1),
+                                ' '
+                              ) + '</h1>';
         end;
         STYLE, COLOR: begin
           if Length(line_split) <= i+1 then
             continue;
 
-          if line_split[i] = STYLE then
-            AppendToStrArray(styles, linesplit[i+1])
-          else
-            AppendToStrArray(colors, linesplit[i+1];
+          tmp := Copy(
+                  line_split[i+1],
+                  1,
+                  Length(line_split[i+1])-1
+                );
 
-          
+          if line_split[i] = STYLE then
+            AppendToStrArray(styles, tmp)
+          else
+            AppendToStrArray(colors, tmp);
+
+
+          HTMLParseSection := HTMLParseSection + Format
+            (
+              '<span class="%s%s">',
+              [
+                Copy(line_split[i], 3, Length(line_split[i])),
+                tmp
+              ]
+            );
+
+          skip := 1;
         end;
         RESET_: begin
+          for j := 0 to (Length(styles) + Length(colors)) - 1 do
+            HTMLParseSection := HTMLParseSection + '</span>';
+
+          if not (preserve_mode = STYLE) then
+            SetLength(styles, 0)
+          else if not (preserve_mode = COLOR) then
+            SetLength(colors, 0);
+
+          if (preserve_mode = '') then continue;
+
+          for tmp in styles do
+            HTMLParseSection := HTMLParseSection + '<span class="' +
+                                preserve_mode +
+                                tmp + '">';
         end;
-        RESET_ALL: ParseSection := ParseSection + STYLE_RESET;
+        RESET_ALL: begin
+          for j := 0 to (Length(styles) + Length(colors)) - 1 do
+            HTMLParseSection := HTMLParseSection + '</span>';
+
+          SetLength(styles, 0);
+          SetLength(colors, 0);
+        end;
         else
         begin
           { Leave out any unimplemented Switches }
           if pos('{$', line_split[i]) = 1 then
             continue;
-          ParseSection := ParseSection + line_split[i] + ' ';
+          HTMLParseSection := HTMLParseSection + line_split[i] + ' ';
         end;
         end;
       end;
 
-      ParseSection := ParseSection + sLineBreak;  end;
+      HTMLParseSection := HTMLParseSection + '<br />' + sLineBreak;
+    end;
+
+    if not ADoChildren then exit;
+
+    for section in ASection.children do
+      HTMLParseSection := HTMLParseSection + HTMLParseSection(section, True);
+
+  end;
 end.
