bf68560c0f32fe8cc9ac99cf16f68decebf7ff5f
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-05-12 09:01:02 +0200

    start htmlparsesection impl

--- sadhtml/src/uSADHTML.pp
+++ sadhtml/src/uSADHTML.pp
@@ -35,14 +35,76 @@ unit uSADHTML;
 {$H+}
 
 interface
-  uses uSADParser;
+  uses SysUtils, StrUtils, Types, uSADParser;
 
   function HTMLParseSection(const ASection: TSection;
                             const ADoChildren: Boolean): String;
 implementation
+  { Private Procedures/Functions }
+  
+  procedure AppendToStrArray(var AArray: TStringDynArray; const AStr: String);
+  begin
+    SetLength(AArray, Length(AArray)+1);
+    AArray[HIGH(AArray)] := AStr;
+  end;
+
+  { Public Procedures/Functions }
+
   function HTMLParseSection(const ASection: TSection;
                             const ADoChildren: Boolean): String;
+  var
+    lines, line_split, styles, colors: TStringDynArray;
+    current_line: String;
+    i, skip: Integer;
+    section: TSection;
   begin
     HTMLParseSection := '';
-  end;
+
+    lines := SplitString(ASection.contents, sLineBreak);
+
+    for current_line in lines do
+    begin
+      line_split := SplitString(current_line, ' ');
+
+      skip := 0;
+      for i := 0 to Length(line_split) - 1 do
+      begin
+        if skip > 0 then
+        begin
+          dec(skip);
+          continue;
+        end;
+
+        case line_split[i] of
+        HEADER: begin
+          skip := Length(line_split);
+        end;
+        SUB_HEADER: begin
+          skip := Length(line_split);
+        end;
+        STYLE, COLOR: begin
+          if Length(line_split) <= i+1 then
+            continue;
+
+          if line_split[i] = STYLE then
+            AppendToStrArray(styles, linesplit[i+1])
+          else
+            AppendToStrArray(colors, linesplit[i+1];
+
+          
+        end;
+        RESET_: begin
+        end;
+        RESET_ALL: ParseSection := ParseSection + STYLE_RESET;
+        else
+        begin
+          { Leave out any unimplemented Switches }
+          if pos('{$', line_split[i]) = 1 then
+            continue;
+          ParseSection := ParseSection + line_split[i] + ' ';
+        end;
+        end;
+      end;
+
+      ParseSection := ParseSection + sLineBreak;  end;
 end.
